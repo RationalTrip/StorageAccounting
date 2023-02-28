3240d72a134307cf8e171e3f58c2c6706d500b7b
Andrii Kozyrkyi <andriy.kozir@gmail.com>
2023-02-28 11:04:27 +0200

    feat(infrastructure): Add key values

---
+++ src/StorageAccounting.Infrastructure/Caching/Statics/EquipmentKeys.cs
@@ -0,0 +1,16 @@
+﻿namespace StorageAccounting.Infrastructure.Caching.Statics
+{
+    internal static class EquipmentKeys
+    {
+        private const string ENTITY_NAME = "Equipment";
+
+        public static string GetAllKey() =>
+            $"_Get_All_{ENTITY_NAME}";
+
+        public static string GetCountKey() =>
+            GetAllKey() + "_Count";
+
+        public static string GetByIdKey(int id) =>
+            $"_Get_{ENTITY_NAME}_By_{id}";
+    }
+}
